412428c946819691ade7c8cfb9f1904aca077fcd
Majid <Mansouridb@gmail.com>
2021-05-02 12:15:35 +0200

    create AppUser

--- src/main/java/org/example/models/AppUser.java
+++ src/main/java/org/example/models/AppUser.java
@@ -1,8 +1,56 @@
 package org.example.models;
 
+import java.rmi.server.UID;
+import java.util.Scanner;
+import java.util.UUID;
+
 public class AppUser {
-    int id;
-    String username;
-    String password;
-    
+    public String username;
+    public int id;
+    private String password;
+    public static int count = 1;
+    public static String input;
+
+    public AppUser(String username, int id, String password) {
+        this.username = username;
+        this.id = getCount(count);
+        this.password = password;
+    }
+
+    public AppUser(String username, String password) {
+        this(username,getCount(count),password);
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public static int getCount(int count) {
+        return AppUser.count;
+    }
+
+    public static void setCount(int count) {
+        AppUser.count = count;
+    }
+
+    public static String getInput() {
+        return input;
+    }
+
+    public static void setInput(String input) {
+        AppUser.input = input;
+    }
 }
